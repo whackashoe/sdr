8209404255fb9b63e140c132d25b92d63843671b
Jett <whackashoe@gmail.com>
2015-07-28 17:58:27 -0500

    make build script debug and ndebug

---
+++ build-debug.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd benchmark
+g++ -std=c++11 bench.cpp -O3 -Weffc++ -Wall -Wextra -pthread -o bench
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd benchmark
-g++ -std=c++11 bench.cpp -O3 -Weffc++ -Wall -Wextra -pthread -o bench
+g++ -std=c++11 bench.cpp -O3 -Weffc++ -Wall -Wextra -pthread -DNDEBUG -o bench
