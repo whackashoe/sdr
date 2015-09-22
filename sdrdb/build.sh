97a25548e3d7245c847729fdbb44611a2f46434a
Jett <whackashoe@gmail.com>
2015-09-21 23:19:55 -0500

    move sdrdb to sdrdb-server

--- sdrdb/build.sh
+++ sdrdb/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-g++ -std=c++11 sdrdbd.cpp -Wall -Wextra -pedantic -o sdrdbd -lsocket++
+g++ -std=c++11 sdrdb-server.cpp -Wall -Wextra -pedantic -o sdrdb-server -lsocket++
 g++ -std=c++11 sdrdb-cli.cpp -Wall -Wextra -pedantic -o sdrdb-cli -lreadline -lsocket++
